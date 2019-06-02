f1049af799eb253e3feca554e9c7cc7b2583b84f
kadeshiro <gaaadaaa16@gmail.com>
2019-06-03 01:34:01 +0900

    appjs

--- functions/app.js
+++ functions/app.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const app = express()
-const history = require('connect-history-api-fallback') 
+// const history = require('connect-history-api-fallback') 
 const path = require('path')
 const resolve = file => path.resolve(__dirname, file)
 const apiRouter = require('./router/api')
 const serverless = require('serverless-http')
 
 
-const serve = path => express.static(resolve(path), {
-  maxAge: 0
-})
+// const serve = path => express.static(resolve(path), {
+//   maxAge: 0
+// })
 
 app.use('/api', apiRouter)
-app.use(history())
-app.use('/data', serve('./data'))
-app.use('/', serve('./public'))
+// app.use(history())
+// app.use('/data', serve('./data'))
+// app.use('/', serve('./public'))
 
 exports.handler = serverless(app)
