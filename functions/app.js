6ee3f646956dff52d85504206e7adce96054748c
kadeshiro <gaaadaaa16@gmail.com>
2019-06-03 01:45:48 +0900

    appjs

--- functions/app.js
+++ functions/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-// const history = require('connect-history-api-fallback') 
+const history = require('connect-history-api-fallback') 
 const path = require('path')
 const resolve = file => path.resolve(__dirname, file)
 const apiRouter = require('./router/api')
