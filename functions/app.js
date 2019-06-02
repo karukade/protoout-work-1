3ec2130f869566f0bee2de8669d945036412f2a8
kadeshiro <gaaadaaa16@gmail.com>
2019-06-03 01:27:19 +0900

    add serverless-http

--- functions/app.js
+++ functions/app.js
@@ -4,6 +4,7 @@ const history = require('connect-history-api-fallback')
 const path = require('path')
 const resolve = file => path.resolve(__dirname, file)
 const apiRouter = require('./router/api')
+const serverless = require('serverless-http')
 
 
 const serve = path => express.static(resolve(path), {
@@ -15,8 +16,4 @@ app.use(history())
 app.use('/data', serve('./data'))
 app.use('/', serve('./public'))
 
-
-
-app.listen(3000, () => {
-  console.log('app listen port 3000')
-})
+exports.handler = serverless(app)
