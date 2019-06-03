f7fbb8671509eec17325146b1411441bc4f96e13
kadeshiro <gaaadaaa16@gmail.com>
2019-06-03 23:36:23 +0900

    modify router mode

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ Vue.use(Router)
 
 export function createRouter() {
   return new Router({
-    mode: 'history',
+    mode: 'hash',
     routes:[
       {path: '/', redirect: {name: 'tag'}},
       {path: '/tag', name: 'tag', component: () => import('./pages/Tag.vue')},
